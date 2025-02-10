a964ddfcfbe2670d212db06dc3003cb2dc7ce067
peggy1008123 <71455962+peggy1008123@users.noreply.github.com>
2025-02-10 19:00:57 +0000

    answer

---
+++ answer.js
@@ -0,0 +1,8 @@
+let challenge1 = 384400 / 3474
+console.log(challenge1.toFixed(1))
+
+let challenge2 = ((41234 + 7854 + 3001) - (40483 + 7780 + 3053)) / (40483 + 7780 + 3053) * 100
+console.log(challenge2.toFixed(1)+' %')
+
+let challenge3 = 8 * 16
+console.log(challenge3)
